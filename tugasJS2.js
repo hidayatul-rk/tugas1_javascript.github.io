820444f17bb543985a95f2a9c2de7ec22621cffe
hidayatul-rk <71365495+hidayatul-rk@users.noreply.github.com>
2020-09-18 11:06:24 +0700

    Add files via upload

---
+++ tugasJS2.js
@@ -0,0 +1,13 @@
+var umur = 27
+var nama = "hidayat"
+var hadir = true
+var pulang = "tidak";
+pulang = null;
+
+var yangHadir = ["yanto", "hidayat", "riftan"]
+
+console.log(typeof umur)
+console.log(typeof nama)
+console.log(typeof hadir)
+console.log(pulang);
+console.log(typeof yangHadir)
